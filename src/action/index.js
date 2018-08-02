d5dca9dd75b3b3fbaa5f57f382040129b92f76a8
Vishrut Shah <vishrutshah15@gmail.com>
2018-08-02 10:17:53 -0700

    Configured Eslint-Prettier

--- src/action/index.js
+++ src/action/index.js
@@ -1,18 +1,16 @@
-export const ADD = 'ADD';
-export const REMOVE = 'REMOVE';
-
+export const ADD = "ADD";
+export const REMOVE = "REMOVE";
 
 export function addToCart(item) {
-    return {
-        type : ADD,
-        item                                // this is same as newItem : newItem in ES6
-    }
+  return {
+    type: ADD,
+    item // this is same as newItem : newItem in ES6
+  };
 }
 
 export function removeFromCart(item) {
-    return {
-        type : REMOVE,
-        item
-    }
+  return {
+    type: REMOVE,
+    item
+  };
 }
-
